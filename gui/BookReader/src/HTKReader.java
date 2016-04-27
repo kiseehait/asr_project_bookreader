a209f2276568eb6c3f744ad703ad020538a132e4
Nat Pavasant <nat@natpavas.me>
2016-04-27 14:37:14 +0700

    Fix things

--- gui/BookReader/src/HTKReader.java
+++ gui/BookReader/src/HTKReader.java
@@ -31,13 +31,16 @@ public class HTKReader implements Runnable {
 
     @Override
     public void run() {
+        launch();
         while (true) {
             try {
                 String s = outputStream.readLine();
                 System.out.println(s);
                 if (s.indexOf("SENT-START") == 0) {
                     int end = s.indexOf("SENT-END");
-                    System.err.println(s.substring(0, end));
+                    String input;
+                    System.err.println(input = s.substring(0, end));
+                    parser(input);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -47,73 +50,93 @@ public class HTKReader implements Runnable {
 
     public static void main(String[] args) {
         HTKReader r = new HTKReader(null);
-        r.launch();
         r.run();
     }
 
     private void parser(String s) {
     	String[] l = s.split(" ");
 
-    	if (l[1].equals("read")) {
-    		control.startReading();
-    	} else if (l[1].equals("stop")) {
-    		control.stopReading();
-    	} else if (l[1].equals("continue")) {
-    		control.startReading();
-    	} else if (l[1].equals("repeat")) {
-    		control.repeating();
-    	} else if (l[1].equals("openTo") || l[1].equals("flipTo") || l[1].equals("goTo") || l[1].equals("goAt")) {
-    		if (l[2].equals("cover")) {
-    			control.goToFrontCover();
-    		} else if (l[2].equals("contents")) {
-    			control.goToContent();
-    		} else if (l[2].equals("preface")) {
-    			control.goToPreface();
-    		} else if (l[2].equals("appendix")) {
-    			control.goToAppendix();
-    		} else if (l[2].equals("reference")) {
-    			control.gotoReference();
-    		} else if (l[2].equals("index")) {
-    			control.goToIndex();
-    		} else if (l[2].equals("backCover")) {
-    			control.goToBackCover();
-    		} else if (l[2].equals("beginChapter")) {
-    			control.goToBeginOfChapter();
-    		} else if (l[2].equals("endChapter")) {
-    			control.goToEndOfChapter();
-    		} else if (l[2].equals("nextChapter")) {
-    			control.nextChapter();
-    		} else if (l[2].equals("previousChapter")) {
-    			control.previousChapter();
-    		} else if (l[2].equals("chapter")) {
-    			String no = "";
-                for (int i = 3; i < l.length - 1; i++) {
-                    if (l[i].equals("one")) {
-                        no += "1";
-                    } else if (l[i].equals("two")) {
-                        no += "2";
-                    } else if (l[i].equals("three")) {
-                        no += "3";
-                    } else if (l[i].equals("four")) {
-                        no += "4";
-                    } else if (l[i].equals("five")) {
-                        no += "5";
-                    } else if (l[i].equals("six")) {
-                        no += "6";
-                    } else if (l[i].equals("seven")) {
-                        no += "7";
-                    } else if (l[i].equals("eight")) {
-                        no += "8";
-                    } else if (l[i].equals("nine")) {
-                        no += "9";
-                    } else if (l[i].equals("zero")) {
-                        no += "0";
-                    }
+        switch (l[1]) {
+            case "read":
+                control.startReading();
+                break;
+            case "stop":
+                control.stopReading();
+                break;
+            case "continue":
+                control.startReading();
+                break;
+            case "repeat":
+                control.repeating();
+                break;
+            case "openTo":
+            case "flipTo":
+            case "goTo":
+            case "goAt":
+                switch (l[2]) {
+                    case "cover":
+                        control.goToFrontCover();
+                        break;
+                    case "contents":
+                        control.goToContent();
+                        break;
+                    case "preface":
+                        control.goToPreface();
+                        break;
+                    case "appendix":
+                        control.goToAppendix();
+                        break;
+                    case "reference":
+                        control.goToReference();
+                        break;
+                    case "index":
+                        control.goToIndex();
+                        break;
+                    case "backCover":
+                        control.goToBackCover();
+                        break;
+                    case "beginChapter":
+                        control.goToBeginOfChapter();
+                        break;
+                    case "endChapter":
+                        control.goToEndOfChapter();
+                        break;
+                    case "nextChapter":
+                        control.nextChapter();
+                        break;
+                    case "previousChapter":
+                        control.previousChapter();
+                        break;
+                    case "chapter":
+                        String no = "";
+                        for (int i = 3; i < l.length; i++) {
+                            if (l[i].equals("one")) {
+                                no += "1";
+                            } else if (l[i].equals("two")) {
+                                no += "2";
+                            } else if (l[i].equals("three")) {
+                                no += "3";
+                            } else if (l[i].equals("four")) {
+                                no += "4";
+                            } else if (l[i].equals("five")) {
+                                no += "5";
+                            } else if (l[i].equals("six")) {
+                                no += "6";
+                            } else if (l[i].equals("seven")) {
+                                no += "7";
+                            } else if (l[i].equals("eight")) {
+                                no += "8";
+                            } else if (l[i].equals("nine")) {
+                                no += "9";
+                            } else if (l[i].equals("zero")) {
+                                no += "0";
+                            }
+                        }
+                        control.goToChapter(Integer.parseInt(no));
+                        break;
                 }
-
-    			control.goToChapter(Integer.parseInt(no));
-    		}
-    	}
+                break;
+        }
 
     }
 }
